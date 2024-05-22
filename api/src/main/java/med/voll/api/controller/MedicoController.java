89f135c90450f66c90239115e5e24ec29ec17fa6
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2024-05-22 16:20:50 -0300

    criando DTO da pesquisa do médico

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,17 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -18,13 +21,26 @@ public class MedicoController {
         private MedicoRepository repository;
 
         @PostMapping
-        @Transactional
-        public void cadastrar(@RequestBody DadosCadastroMedico dados){
+        @Transactional                     // preciso do @valid aqui para validar o que coloquei em cada objeto
+        public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
             repository.save(new Medico(dados));
-
-
         }
 
+        @GetMapping
+        public List<DadosListagemMedico> buscarTodos(){ //preciso retornar um DTO, pois não quero retonar todos os atributos quando criado
+                                                                                                        //e posso passar apenas o que desejar
+                return repository.findAll().stream().map(DadosListagemMedico::new).toList();//converto o meu DTO em uma entity para buscar no banco
+        }
 
- 
+        /*
+        @GetMapping("{id}")
+        public DadosListagemMedico buscarPorId(@PathVariable Long id){
+                return repository
+                        .findById(id)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        }
+*/
+
+
+
 }
--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,9 +1,28 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico (String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
-
+public record DadosCadastroMedico (
+        @NotBlank //verifica se o campo é vazio ou nullo (só para campos String)
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")//expressão regular que vai exigir um númere 4 a 6 digitos
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid //essa anotação esta dizendo para entrar dentro do objeto Dados Endereço e, validar cada atribute dele
+        DadosEndereco endereco) {
 
 
 }
